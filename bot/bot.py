f9e6340254b074c2437a20c90012342ef506c416
manuhortet <manuhortet@gmail.com>
2019-03-29 11:39:03 +0100

    add continuous mode using /lyrics

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,15 @@
+# TODO:
+# Add automatic mode
+
 import telegram
+import time
 import logging
 from telegram.ext import CommandHandler, ConversationHandler, Filters, MessageHandler, Updater
 from telegram.ext.dispatcher import run_async
 from credentials.credentials import token
 from bot.login import USERNAME, PASSWORD, username, password, start, cancel
+
+live_lyrics = False
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -11,7 +17,7 @@ bot = telegram.Bot(token=token)
 
 
 @run_async
-def lyrics(bot, update):
+def current_song(bot, update):
     from bot.login import user_spotify
     song = user_spotify.get_song()
     lyrics = user_spotify.get_lyrics()
@@ -19,6 +25,33 @@ def lyrics(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="\U0001F3B6 *Lyrics for*: " + song,
                     parse_mode=telegram.ParseMode.MARKDOWN)
     bot.sendMessage(chat_id=update.message.chat_id, text=lyrics)
+
+
+@run_async
+def lyrics(bot, update):
+    from bot.login import user_spotify
+    global live_lyrics
+    live_lyrics = True
+    song = ''
+
+    while live_lyrics:
+        start_time = time.time()
+        new_song = user_spotify.get_song()
+        if song != new_song:
+            song = new_song
+            lyrics = user_spotify.get_lyrics()
+            bot.sendMessage(chat_id=update.message.chat_id, text="\U0001F3B6 *Lyrics for*: " + song,
+                            parse_mode=telegram.ParseMode.MARKDOWN)
+            bot.sendMessage(chat_id=update.message.chat_id, text=lyrics)
+        time.sleep(15.0 - ((time.time() - start_time) % 15.0))
+
+
+@run_async
+def stop(bot, update):
+    global live_lyrics
+    bot.sendMessage(chat_id=update.message.chat_id, text="Ok... \U0001F636")
+    live_lyrics = False
+    return -1
 
 
 def main():
@@ -34,7 +67,9 @@ def main():
         },
         fallbacks=[CommandHandler('cancel', cancel)]))
 
+    dispatcher.add_handler(CommandHandler('current_song', current_song))
     dispatcher.add_handler(CommandHandler('lyrics', lyrics))
+    dispatcher.add_handler(CommandHandler('stop', stop))
 
     updater.start_polling()
     updater.idle()
